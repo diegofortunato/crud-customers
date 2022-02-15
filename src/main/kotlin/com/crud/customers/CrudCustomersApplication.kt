f4c0f5dabafd6a20f03a33f03ac80da53f7f0ef0
Diego Fortunato <diego.candido@picpay.com>
2022-02-15 18:31:05 -0300

    Primeiro commit da estrutura inicial

---
+++ src/main/kotlin/com/crud/customers/CrudCustomersApplication.kt
@@ -0,0 +1,11 @@
+package com.crud.customers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudCustomersApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudCustomersApplication>(*args)
+}
---
+++ src/test/kotlin/com/crud/customers/CrudCustomersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.crud.customers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudCustomersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
